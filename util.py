2e2e419e60ec7976ea522cbeeaf3b256b3ca36ff
Yuanming Hu <yuanmhu@gmail.com>
2018-05-13 16:05:43 -0400

    merged async_task_manager.py into util.py

--- util.py
+++ util.py
@@ -588,3 +588,79 @@ def linearProPhotoRGB2Lab(img):
   xyz = ProPhotoRGB2XYZ(img)
   lab = color.xyz2lab(xyz)
   return lab
+
+import threading
+import time
+
+
+class AsyncTaskManager:
+
+  def __init__(self, target, args=(), kwargs={}):
+    self.target = target
+    self.args = args
+    self.kwargs = kwargs
+    self.condition = threading.Condition()
+    self.result = None
+    self.thread = threading.Thread(target=self.worker)
+    self.stopped = False
+    self.thread.daemon = True
+    self.thread.start()
+
+  def worker(self):
+    while True:
+      self.condition.acquire()
+      while self.result is not None:
+        if self.stopped:
+          self.condition.release()
+          return
+        self.condition.notify()
+        self.condition.wait()
+      self.condition.notify()
+      self.condition.release()
+
+      result = (self.target(*self.args, **self.kwargs),)
+
+      self.condition.acquire()
+      self.result = result
+      self.condition.notify()
+      self.condition.release()
+
+  def get_next(self):
+    self.condition.acquire()
+    while self.result is None:
+      self.condition.notify()
+      self.condition.wait()
+    result = self.result[0]
+    self.result = None
+    self.condition.notify()
+    self.condition.release()
+    return result
+
+  def stop(self):
+    while self.thread.is_alive():
+      self.condition.acquire()
+      self.stopped = True
+      self.condition.notify()
+      self.condition.release()
+
+
+def test_async_task_manager():
+  def task():
+    print('begin sleeping...')
+    time.sleep(1)
+    print('end sleeping.')
+    task.i += 1
+    print('returns', task.i)
+    return task.i
+
+  task.i = 0
+
+  async = AsyncTaskManager(task)
+  t = time.time()
+  for i in range(5):
+    ret = async.get_next()
+    # ret = task()
+    print('got', ret)
+    time.sleep(1)
+  async.stop()
+  print(time.time() - t)
